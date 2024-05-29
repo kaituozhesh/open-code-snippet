9aba5b4dbac3f3d060b405926f153c43628c0312
林俊豪 <linjh@olymtech.com>
2024-05-29 16:26:28 +0800

    feat(basic) : StreamAPI(嵌套Map)

--- open-code-snippet-basic/src/main/java/com/ityizhan/open/basic/java8/StreamApiExample.java
+++ open-code-snippet-basic/src/main/java/com/ityizhan/open/basic/java8/StreamApiExample.java
@@ -2,8 +2,10 @@ package com.ityizhan.open.basic.java8;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,9 +21,13 @@ public class StreamApiExample {
     public static void main(String[] args) {
         // 1. 根据某个字段分组后取另一个字段集合 EXP: Map<String, List<Integer>> key:10001 values:[1, 2, 3]
         Map<String, List<Integer>> customerTypeMap = groupByFieldCollectValues();
+
+        // 2. 嵌套Map EXP: 省->市->区  浙江省 -> { 宁波市 -> {鄞州区, 海曙区}, 台州市 -> {温岭市, 临海市} }
+        Map<String, Map<String, List<String>>> nestedMap = nestedMap();
     }
 
     public static Map<String, List<Integer>> groupByFieldCollectValues() {
+        System.out.println("====groupByFieldCollectValues====");
         @Data
         @AllArgsConstructor
         class SettlementDate {
@@ -41,5 +47,40 @@ public class StreamApiExample {
                 );
         System.out.println(customerTypeMap);
         return customerTypeMap;
+    }
+
+    public static Map<String, Map<String, List<String>>> nestedMap() {
+        System.out.println("====nestedMap====");
+        @Getter
+        @AllArgsConstructor
+        class Address {
+            String province;
+            String city;
+            String area;
+        }
+
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new Address("浙江省", "宁波市", "鄞州区"));
+        addresses.add(new Address("浙江省", "宁波市", "海曙区"));
+        addresses.add(new Address("浙江省", "台州市", "温岭市"));
+        addresses.add(new Address("浙江省", "台州市", "临海市"));
+
+        // 没有Java8之前
+        Map<String, Map<String, List<String>>> nestedMap = new HashMap<>();
+        for (Address address : addresses) {
+            nestedMap.computeIfAbsent(address.getProvince(), e -> new HashMap<>())
+                    .computeIfAbsent(address.getCity(), e -> new ArrayList<>())
+                    .add(address.getArea());
+        }
+        System.out.println("java:" + nestedMap);
+
+        // Java8
+        nestedMap = addresses.stream().collect(
+                Collectors.groupingBy(Address::getProvince,
+                        Collectors.groupingBy(Address::getCity, Collectors.mapping(Address::getArea, Collectors.toList())))
+        );
+        System.out.println("java8:" + nestedMap);
+
+        return nestedMap;
     }
 }
