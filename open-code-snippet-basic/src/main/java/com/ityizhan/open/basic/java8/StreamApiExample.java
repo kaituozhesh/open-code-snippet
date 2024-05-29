51b3543d89a4502d16a03a365d660c7a45e020ca
林俊豪 <linjh@olymtech.com>
2024-05-29 16:45:06 +0800

    feat(basic) : StreamAPI(根据字段分组计算组内BigDecimal总和)

--- open-code-snippet-basic/src/main/java/com/ityizhan/open/basic/java8/StreamApiExample.java
+++ open-code-snippet-basic/src/main/java/com/ityizhan/open/basic/java8/StreamApiExample.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,10 +25,13 @@ public class StreamApiExample {
 
         // 2. 嵌套Map EXP: 省->市->区  浙江省 -> { 宁波市 -> {鄞州区, 海曙区}, 台州市 -> {温岭市, 临海市} }
         Map<String, Map<String, List<String>>> nestedMap = nestedMap();
+
+        // 3.1.BigDecimal字段累加 3.2.根据字段分组计算组内BigDecimal总和 EXP: {10002=465, 10001=127}
+        Map<String, BigDecimal> reduceAdd = reduceAdd();
     }
 
     public static Map<String, List<Integer>> groupByFieldCollectValues() {
-        System.out.println("====groupByFieldCollectValues====");
+        System.out.println("\n====groupByFieldCollectValues====");
         @Data
         @AllArgsConstructor
         class SettlementDate {
@@ -50,7 +54,7 @@ public class StreamApiExample {
     }
 
     public static Map<String, Map<String, List<String>>> nestedMap() {
-        System.out.println("====nestedMap====");
+        System.out.println("\n====nestedMap====");
         @Getter
         @AllArgsConstructor
         class Address {
@@ -82,5 +86,39 @@ public class StreamApiExample {
         System.out.println("java8:" + nestedMap);
 
         return nestedMap;
+    }
+
+    private static Map<String, BigDecimal> reduceAdd() {
+
+        System.out.println("\n====reduceAdd====");
+
+        @Getter
+        @AllArgsConstructor
+        class FeeItem {
+            String orderNo;
+            String feeId;
+            BigDecimal amount;
+        }
+
+        List<FeeItem> feeItems = new ArrayList<>();
+        feeItems.add(new FeeItem("10001", "100010001", BigDecimal.valueOf(10)));
+        feeItems.add(new FeeItem("10001", "100010002", BigDecimal.valueOf(13)));
+        feeItems.add(new FeeItem("10001", "100010003", BigDecimal.valueOf(104)));
+
+        feeItems.add(new FeeItem("10002", "100020001", BigDecimal.valueOf(111)));
+        feeItems.add(new FeeItem("10002", "100020002", BigDecimal.valueOf(222)));
+        feeItems.add(new FeeItem("10002", "100020003", BigDecimal.valueOf(132)));
+
+        BigDecimal sum = feeItems.stream().map(FeeItem::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("3.1:TotalSum:" + sum);
+
+        Map<String, BigDecimal> reduceAdd = feeItems.stream().collect(
+                Collectors.groupingBy(FeeItem::getOrderNo,
+                        Collectors.mapping(FeeItem::getAmount, Collectors.reducing(BigDecimal.ZERO, BigDecimal::add)))
+        );
+
+        System.out.println("3.2:GroupSum:" + reduceAdd);
+
+        return reduceAdd;
     }
 }
